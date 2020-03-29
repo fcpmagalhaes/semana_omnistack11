94240e79988958774f2a062c7ffaf1a7d4766ff1
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-03-29 10:27:06 -0300

    update gitignore

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logon from './pages/Logon';
+import './global.css';
+function App() {
+  return (
+    <div>
+      <Logon></Logon>
+    </div>
+  );
+}
+
+export default App;
