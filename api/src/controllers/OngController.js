cf7a9bdbe5b7b1e2ed15d1668a1b261d37ca051e
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-09-15 12:22:55 -0300

    create integration and unit test to ong

--- api/src/controllers/OngController.js
+++ api/src/controllers/OngController.js
@@ -1,10 +1,17 @@
 const connection = require("../database/connection");
-const crypto = require("crypto");
+const generateUniqueId = require("../utils/generateUniqueId");
 
 module.exports = {
+  async index(request, response) {
+    const ongs = await connection("ongs").select("*");
+
+    return response.json(ongs);
+  },
+
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString("HEX");
+    
+    const id = generateUniqueId();
 
     await connection("ongs").insert({
       id,
@@ -18,9 +25,5 @@ module.exports = {
     return response.json({ id });
   },
 
-  async index(request, response) {
-    const ongs = await connection("ongs").select("*");
-
-    return response.json(ongs);
-  },
+  
 };
